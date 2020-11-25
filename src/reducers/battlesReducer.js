44458a65c56ebc5611ee04ebbd49858bf6fe309e
viraj-rapidops <viraj.sathvara@rapidops.com>
2020-11-26 00:06:15 +0530

    fix for the intial stage withoout data

--- src/reducers/battlesReducer.js
+++ src/reducers/battlesReducer.js
@@ -1,6 +1,9 @@
 
 const initialState = {
-  battle: {}
+  battle: {},
+  locations: [],
+  kings: [],
+  types: []
 };
 const BattlesReducer = (state = initialState, action) => {
   switch (action.type) {
